1b65ada776fa04db6565320c570873163089791e
FelipeFVieira <devfelipefranca@gmail.com>
2024-05-02 21:55:58 -0300

    feat: add Spring Project

---
+++ springbandapi/src/main/java/com/restapi/springbandapi/springbandapi/SpringbandapiApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.springbandapi.springbandapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbandapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbandapiApplication.class, args);
+	}
+
+}
---
+++ springbandapi/src/test/java/com/restapi/springbandapi/springbandapi/SpringbandapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.springbandapi.springbandapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbandapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
